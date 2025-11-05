2b6de93e6aa54d30456ff4d9cec585e2fa7fd1e0
Kyle Montiel-Berner <kyle_montielberner@outlook.com>
2025-11-05 11:43:15 -0600

    Refactor: combine 6 functions into 2
    make code cleaner by not repeating myself as much. Convert function for each point into one function for each team.

--- script.js
+++ script.js
@@ -13,43 +13,22 @@ let awayRunningScore = 0;
 
 function playGame() {
 
-onePointHome.addEventListener('click', addOneHome);
-twoPointHome.addEventListener('click', addTwoHome);
-threePointHome.addEventListener('click', addThreeHome);
-onePointAway.addEventListener('click', addOneAway);
-twoPointAway.addEventListener('click', addTwoAway);
-threePointAway.addEventListener('click', addThreeAway);
+onePointHome.addEventListener('click', () => updateHomePoints(1));
+twoPointHome.addEventListener('click', () => updateHomePoints(2));
+threePointHome.addEventListener('click', () => updateHomePoints(3));
+onePointAway.addEventListener('click', () => updateAwayPoints(1));
+twoPointAway.addEventListener('click', () => updateAwayPoints(2));
+threePointAway.addEventListener('click', () => updateAwayPoints(3));
 
-    function addOneHome() {
-        homeRunningScore += 1;
+    function updateHomePoints(points) {
+        homeRunningScore += points;
         return homeTotal.textContent = homeRunningScore;
     };
 
-    function addTwoHome() {
-        homeRunningScore += 2;
-        return homeTotal.textContent = homeRunningScore;
-    };
-
-    function addThreeHome() {
-        homeRunningScore += 3;
-        return homeTotal.textContent = homeRunningScore;
-    };
-
-    function addOneAway() {
-        awayRunningScore += 1;
+    function updateAwayPoints(points) {
+        awayRunningScore += points
         return awayTotal.textContent = awayRunningScore;
     };
-
-    function addTwoAway() {
-        awayRunningScore += 2;
-        return awayTotal.textContent = awayRunningScore;
-    };
-
-    function addThreeAway() {
-        awayRunningScore += 3;
-        return awayTotal.textContent = awayRunningScore;
-    };
-
 };
 
 playGame()
